44a2bafd8456a12e0ae0531c6caa81a2844b7d88
moonggae <egchoi.dev@gmail.com>
2024-10-08 00:39:08 +0900

    add image cache

--- app/src/main/java/com/ccc/ncs/NcsApplication.kt
+++ app/src/main/java/com/ccc/ncs/NcsApplication.kt
@@ -2,17 +2,31 @@ package com.ccc.ncs
 
 import android.app.Application
 import androidx.work.Configuration
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
 import com.ccc.ncs.download.DownloadCompletedWorker
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class NcsApplication: Application(), Configuration.Provider {
+class NcsApplication: Application(), Configuration.Provider, ImageLoaderFactory {
     @Inject
     lateinit var downloadCompletedWorkerFactory: DownloadCompletedWorker.Factory
 
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder()
             .setWorkerFactory(downloadCompletedWorkerFactory)
+            .build()
+
+    override fun newImageLoader(): ImageLoader =
+        ImageLoader.Builder(this)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve(".imageCache"))
+                    .minimumMaxSizeBytes(30 * 1024 * 1024)
+                    .maximumMaxSizeBytes(100 * 1024 * 1024)
+                    .build()
+            }
             .build()
 }
