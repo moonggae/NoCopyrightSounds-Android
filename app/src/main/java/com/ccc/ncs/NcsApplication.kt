92715f30a2026ed301d7fbbebe0dbb48ae20644a
moonggae <egchoi.dev@gmail.com>
2024-10-18 19:57:22 +0900

    firebase analytics, crashlytics 활성화 변수 추가

--- app/src/main/java/com/ccc/ncs/NcsApplication.kt
+++ app/src/main/java/com/ccc/ncs/NcsApplication.kt
@@ -6,6 +6,8 @@ import coil.ImageLoader
 import coil.ImageLoaderFactory
 import coil.disk.DiskCache
 import com.ccc.ncs.download.DownloadCompletedWorker
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -29,4 +31,15 @@ class NcsApplication: Application(), Configuration.Provider, ImageLoaderFactory 
                     .build()
             }
             .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        configureFirebaseAnalytics()
+    }
+
+    private fun configureFirebaseAnalytics() {
+        val enabled = BuildConfig.FIREBASE_ANALYTICS_ENABLED.toBooleanStrictOrNull() ?: false
+        FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(enabled)
+        FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = enabled
+    }
 }
